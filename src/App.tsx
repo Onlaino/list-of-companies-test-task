be2b23e94607e5a68e29298b095416bcf1d7f498
Oleg Vasilev <v.oleg2908@gmail.com>
2024-10-05 00:46:43 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import './App.css';
-// import { Table } from './components/Table/Table';
+
 import { MainPage } from './pages/MainPage';
 
-function App() {
+export const App = () => {
 	return (
 		<div className='App'>
-			{/* <Table /> */}
 			<MainPage />
 		</div>
 	);
-}
+};
 
-export default App;
